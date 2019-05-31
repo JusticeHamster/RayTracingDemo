fec57a0682db3d06f66e04f739fa96134253d389
wzy <wuzy.nju@gmail.com>
2019-06-01 00:20:16 +0800

    update cube params and init

--- object/cube.cpp
+++ object/cube.cpp
@@ -3,14 +3,34 @@
 
 #include "opengl/opengl_header.hpp"
 
-cube::cube()
+cube::cube(glm::vec3 axis_x, glm::vec3 axis_y, glm::vec3 axis_z, glm::vec3 extend, glm::vec3 center)
 {
-
+    this->axis_x = axis_x;
+    this->axis_y = axis_y;
+    this->axis_z = axis_z;
+    this->extend = extend;
+    this->center = center;
+    init_vertex();
 }
 
 cube::~cube()
 {
+}
 
+void cube::init_vertex()
+{
+    vertex[0] = extend * (-axis_x - axis_y + axis_z);
+    vertex[1] = extend * (-axis_x + axis_y + axis_z);
+    vertex[2] = extend * ( axis_x - axis_y + axis_z);
+    vertex[3] = extend * ( axis_x + axis_y + axis_z);
+    vertex[4] = extend * ( axis_x - axis_y - axis_z);
+    vertex[5] = extend * ( axis_x + axis_y - axis_z);
+    vertex[6] = extend * (-axis_x - axis_y - axis_z);
+    vertex[7] = extend * (-axis_x + axis_y - axis_z);
+    for (int i= 0;i<8;i++){
+        vertex[i] /= 2; 
+        vertex[i] += center; 
+    }
 }
 
 glm::vec3 cube::normal(glm::vec3 point) const
@@ -20,61 +40,27 @@ glm::vec3 cube::normal(glm::vec3 point) const
 
 void cube::draw(glm::vec3 pos, glm::vec3 di) const
 {
-    glBegin(GL_QUAD_STRIP);         //填充凸多边形
-    glColor3f(1, 0, 0);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor3f(1, 1, 0);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(0, 1, 0);
-    glVertex3f(1.0f, 0.0f, 0.0f);
-    glColor3f(0, 1, 1);
-    glVertex3f(1.0f, 1.0f, 0.0f);
-    glColor3f(1, 0, 0);
-    glVertex3f(1.0f, 0.0f, -1.0f);
-    glColor3f(1, 1, 0);
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glColor3f(0, 1, 0);
-    glVertex3f(0.0f, 0.0f, -1.0f);
-    glColor3f(0, 1, 1);
-    glVertex3f(0.0f, 1.0f, -1.0f);
-    glColor3f(1, 0, 0);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor3f(1, 1, 0);
-    glVertex3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_QUAD_STRIP); //填充凸多边形
+    for (int i= 0;i<8;i++)
+        glVertex3f(vertex[i].x, vertex[i].y, vertex[i].z);
     glEnd();
     glBegin(GL_QUAD_STRIP);
-    glColor3f(0, 0, 1);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glColor3f(1, 0, 1);
-    glVertex3f(1.0f, 0.0f, 0.0f);
-    glColor3f(0, 1, 0);
-    glVertex3f(0.0f, 0.0f, -1.0f);
-    glColor3f(1, 0, 0);
-    glVertex3f(1.0f, 0.0f, -1.0f);
-    glColor3f(1, 1, 0);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glColor3f(1, 0, 1);
-    glVertex3f(1.0f, 1.0f, 0.0f);
-    glColor3f(0, 0, 1);
-    glVertex3f(0.0f, 1.0f, -1.0f);
-    glColor3f(1, 0, 0);
-    glVertex3f(1.0f, 1.0f, -1.0f);
+    int t[8] = {0,2,6,4,1,3,7,5};
+    for (int i= 0;i<8;i++)
+        glVertex3f(vertex[t[i]].x, vertex[t[i]].y, vertex[t[i]].z);
     glEnd();
 }
 
 void cube::rotate()
 {
-
 }
 
 void cube::move()
 {
-
 }
 
 void cube::scale()
 {
-
 }
 
 float cube::intersect(ray &in) const
