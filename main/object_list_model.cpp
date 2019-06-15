0d6a25e03cb6092c3ef1ac4951a421de06ec5ca9
RailgunHamster <31252310+RailgunHamster@users.noreply.github.com>
2019-06-15 21:34:59 +0800

    Update object_list_model.cpp

--- main/object_list_model.cpp
+++ main/object_list_model.cpp
@@ -13,17 +13,43 @@
 
 static loader &ldr = loader::instance;
 
+// reference: https://stackoverflow.com/questions/2409504/using-c-filestreams-fstream-how-can-you-determine-the-size-of-a-file
+inline std::streampos get_file_size(const char *path)
+{
+    std::streampos fsize = 0;
+    std::ifstream save_file(path, std::ios::binary);
+
+    fsize = save_file.tellg();
+    save_file.seekg(0, std::ios::end);
+    fsize = save_file.tellg() - fsize;
+    save_file.close();
+
+    return fsize;
+}
+
 void object_list_model::load_data()
 {
     scene &scn = ldr.get_running_scene();
+    auto fsize = get_file_size("object_list.data");
+    if (fsize == 0) {
+        qDebug(R"("object_list.data" file not found.)");
+        return;
+    }
+    std::ifstream save_file("object_list.data", std::ios::binary);
+    buffer buf(static_cast<uint64_t>(fsize));
+    save_file.read(reinterpret_cast<char *>(&buf[0]), fsize);
     scn.deserialize(buf);
 }
 
 void object_list_model::save_data()
 {
     scene &scn = ldr.get_running_scene();
-    std::ofstream save_file("object_list.data");
-    save_file << scn.serialize();
+    auto buf = scn.serialize();
+    if (buf.size() != 0) {
+        std::ofstream save_file("object_list.data", std::ios::binary);
+        save_file.write(reinterpret_cast<const char *>(&buf[0]), static_cast<long long>(buf.size()));
+        save_file.close();
+    }
 }
 
 object_list_model::object_list_model(QObject *parent): QAbstractListModel(parent)
